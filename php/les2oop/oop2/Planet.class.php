75a2c2f95efa2a45ddfb44accb16c373f5b854b9
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2023-02-17 09:37:46 +0100

    classes, nginx , config

---
+++ php/les2oop/oop2/Planet.class.php
@@ -0,0 +1,36 @@
+<?
+
+class Planet
+{
+
+    private $name;
+    private $diameter;
+    private $distanceFromSun;
+
+    public function __construct(String $name, int $diameter, int $distanceFromSun)
+    {
+        $this->name = $name;
+        $this->diameter = $diameter;
+        $this->distanceFromSun = $distanceFromSun;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDiameter()
+    {
+        return $this->diameter;
+    }
+
+    public function getDistanceFromSun()
+    {
+        return $this->distanceFromSun;
+    }
+
+    public function getOmtrek()
+    {
+        return pi() * $this->getDiameter();
+    }
+}
---
+++ php/les3/includes/Bird.class.php
@@ -0,0 +1,15 @@
+<?
+
+class Bird extends Animal
+{
+
+    public function fly()
+    {
+        print $this->name . " is flying";
+    }
+
+    public function setNoise(String $noise)
+    {
+        $this->noise = $noise;
+    }
+}
